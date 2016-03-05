5e202407f1b183d67758d9fb9214eb465adda102
George John <george@renfos.com>
2016-03-05 16:09:01 +0530

    Bugfix

--- src/Lavalite/Calendar/Http/routes.php
+++ src/Lavalite/Calendar/Http/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 // Admin routes for module
-Route::group(['prefix' => trans_setlocale().'/admin/calendar', 'middleware' => ['web', 'auth.role:admin,superuser']], function () {
+Route::group(['prefix' => trans_setlocale().'/admin/calendar', 'middleware' => ['web', 'auth.role:admin|superuser']], function () {
     Route::resource('calendar', 'CalendarAdminController');
     Route::get('calendar/ajax/list', 'CalendarAdminController@calendarList');
 });
